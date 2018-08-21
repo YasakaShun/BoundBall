7852608b7158b1913136eacab184063e4cee19b9
etsuko <0806ozaki@gmail.com>
2018-08-21 13:06:12 +0900

    初コミ

---
+++ Assets/BallController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+
+    public float speedX = 10.0f;
+    public float speedY = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        var vel = new Vector3(speedX, speedY, 0.0f);
+        this.GetComponent<Rigidbody>().AddForce(
+            vel,
+            ForceMode.VelocityChange);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
