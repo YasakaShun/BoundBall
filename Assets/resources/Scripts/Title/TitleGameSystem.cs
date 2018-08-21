6b2f26bdc8684fa25f80a1e1f3b7b307e109cfe2
YasakaShun <sdshvedujbfxye@gmail.com>
2018-08-21 22:44:13 +0900

    Titleシーン仮作成

---
+++ Assets/resources/Scripts/Title/TitleGameSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleGameSystem : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Level0");
+        // Application.LoadLevel("Level0");
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+    }
+}
