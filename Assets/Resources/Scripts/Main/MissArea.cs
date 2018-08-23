d913bf66c5e94691db860a4b3093de421df1dfa0
YasakaShun <sdshvedujbfxye@gmail.com>
2018-08-23 23:37:01 +0900

    ボールが落下したらミス(とりあえずシーンリスタート)

---
+++ Assets/Resources/Scripts/Main/MissArea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MissArea : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            Debug.Log("Restart");
+            // TODO: 今いるシーンをリスタートする
+            SceneManager.LoadScene("Level0");
+        }
+    }
+}
