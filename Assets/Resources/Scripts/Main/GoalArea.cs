8baa77a82d4583342a652df6d5f306d79b58e46e
YasakaShun <sdshvedujbfxye@gmail.com>
2018-08-25 15:21:00 +0900

    ゴール作成中

---
+++ Assets/Resources/Scripts/Main/GoalArea.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalArea : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            Debug.Log("Goal!!");
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Main/MoveLR.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLR : MonoBehaviour
+{
+    private const float Accel = 1000.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //力を加える
+        this.GetComponent<Rigidbody>().AddForce(
+            Vector3.right * Input.GetAxisRaw("Horizontal") * Accel,
+            ForceMode.Impulse
+            );
+    }
+}
