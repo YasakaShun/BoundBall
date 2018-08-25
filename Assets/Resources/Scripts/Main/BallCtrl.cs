fb81d8a013e18c0fff867a20711bb774a5a4dcf1
YasakaShun <sdshvedujbfxye@gmail.com>
2018-08-25 18:08:41 +0900

    Prefabs フォルダの位置を修正

---
+++ Assets/Resources/Scripts/Main/BallCtrl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallCtrl : MonoBehaviour
+{
+    public float speedX = 10.0f;
+    public float speedY = 0.0f;
+    public float maxMagnitude = 50.0f;
+    public bool displayMagnitude = false;
+
+    public void OnGoal()
+    {
+        // バウンドしないようにする
+        this.GetComponent<Collider>().material.bounciness = 0.01f;
+    }
+
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().AddForce(
+            new Vector3(speedX, speedY, 0.0f),
+            ForceMode.VelocityChange
+            );
+
+    }
+
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        var rigidbody = this.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxMagnitude);
+        if (displayMagnitude)
+        {
+            Debug.Log(rigidbody.velocity.magnitude);
+        }
+    }
+}
