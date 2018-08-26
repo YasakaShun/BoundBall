a3442d85098e06127bee3574d05c0f4d2ab49b5a
YasakaShun <sdshvedujbfxye@gmail.com>
2018-08-26 20:11:52 +0900

    ステージ選択仮実装

---
+++ Assets/Resources/Scripts/Title/LevelBtn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelBtn : MonoBehaviour
+{
+    public int Level = 1;
+
+    public void Awake()
+    {
+        this.GetComponent<Button>().onClick.AddListener(OnClick);
+        this.GetComponentInChildren<Text>().text = Level.ToString();
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Level" + Level.ToString());
+    }
+}
