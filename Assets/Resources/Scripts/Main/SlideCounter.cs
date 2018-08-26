f10c744283396e000102b64e332a146c18e67c5a
YasakaShun <sdshvedujbfxye@gmail.com>
2018-08-26 22:54:01 +0900

    全ステージに必要な要素をまとめてプレハブ化

---
+++ Assets/Resources/Scripts/Main/SlideCounter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SlideCounter
+{
+    private const float Speed = 15.0f;
+
+    public float ScrollValue { get; private set; }
+
+    public SlideCounter()
+    {
+        ScrollValue = 0.0f;
+    }
+
+    public void Update()
+    {
+        if (MainSceneManager.Instance.State != MainSceneManager.SceneState.Main)
+        {
+            return;
+        }
+
+        if (MainSceneManager.Instance.TimeCtrl.IsStop)
+        {
+            return;
+        }
+
+        ScrollValue += Input.GetAxisRaw("Horizontal") * Speed * Time.deltaTime;
+    }
+}
